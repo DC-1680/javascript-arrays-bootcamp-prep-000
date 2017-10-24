1f6933de51aa6ef7f6ce3ab699f6e0d37d92568a
Daryl Concha <concha.daryl@gmail.com>
2017-10-24 21:33:17 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,32 @@
+
+var chocolateBars = [`snickers`, `hundred grand`, `kitkat`, `skittles`];
+var array = [1, 2, 3];
+var element = 'foo';
+
+function addElementToBeginningOfArray(array, element) {
+  return([element,...array]);
+  };
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift('foo');
+  return array;
+  };
+
+function addElementToEndOfArray(array, element){
+  // takes two arguments
+  // adds an element to the end of the array
+  //Should not alter the original array
+  return([...array, 'foo']);
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  /* takes two arguments
+  adds an element to the end of the array
+  Should alter the original array */
+  array.push(element);
+  return array;
+}
+
+function accessElementInArray(array, index){
+  console.log(array[2]);
+}
